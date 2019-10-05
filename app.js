17df472346061b68f0e7e85ee73f28ceee06a75f
Ermoshka <ekingsabyrzhanov@gmail.com>
2019-10-06 01:40:01 +0600

    made

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const nunjucks = require("nunjucks")
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"))
+    // res.sendFile(path.join(__dirname, "index.html"))
+    res.render("index.html")
 })
 
 app.listen(process.env.PORT)
